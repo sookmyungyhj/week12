9ed96cb93f00bafb69cd61460d38e0225cb3534c
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-11-19 15:57:59 +0900

    2

--- main.c
+++ main.c
@@ -3,25 +3,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(void) {
+	int i=0;
+	char str[4];
 	
-	FILE*fp;
-	char str[100];
-	int i;
+	str[0] = 'a';
+	str[1] = 'b';
+	str[2] = 'c';
+	str[3] = '\0';
+
+	printf("%s\n",str);
 	
-	fp=fopen("sample.txt","w");
-	if(fp == NULL)
-	{
-		return -1;
+	while(str[i] != '\0'){ //현재 문자 (str[i])가 맨 끝이 아님 
+		printf("%c",str[i]); //printf로 문자 하나 출력; 
+		i++; //i를 1증가 
 	}
-	for (i=0;i<3;i++){
-	
-	printf("input a word:");
-	scanf("%s", str);
-	fprintf(fp,"%s\n",str);
-	}
-	
-	fclose(fp);
 	
 	return 0;
 }
