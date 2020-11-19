4dbc32ac7fc7e89edf2fd6c058826d4fda88f4d5
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-11-19 16:14:05 +0900

    6-1
    하나씩

--- main.c
+++ main.c
@@ -4,20 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int i=0;
-	char str[4];
 	
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
+	FILE*fp = NULL;
+	char c;
+	
+	fp=fopen("sample.txt","r");
 
-	printf("%s\n",str);
-	
-	while(str[i] != '\0'){ //현재 문자 (str[i])가 맨 끝이 아님 
-		printf("%c",str[i]); //printf로 문자 하나 출력; 
-		i++; //i를 1증가 
+	if(fp == NULL)
+		printf("파일을 못열음\n");
+
+	while((c=fgetc(fp)) != EOF){
+		printf("%c",c);
+//		putchar(c);
 	}
 	
-	return 0;
+	fclose(fp);
 }
